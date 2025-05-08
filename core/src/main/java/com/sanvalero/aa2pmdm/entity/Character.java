54502ea8c8a0b0941ea7af5603b988cbefb57a49
Javibu13 <javiersanzmoline@gmail.com>
2025-05-08 19:21:41 +0200

    Create Enity class to define a more basic class to be used in inheritance of 2d elements

--- core/src/main/java/com/sanvalero/aa2pmdm/entity/Character.java
+++ core/src/main/java/com/sanvalero/aa2pmdm/entity/Character.java
@@ -8,51 +8,37 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-public abstract class Character {
-    protected TextureRegion currentFrame;
-    protected Vector2 position;
+public abstract class Character extends Entity {
     protected Vector2 velocity;
-    public Rectangle collisionShape;
     public Rectangle collisionShapeRight;
     public Rectangle collisionShapeLeft;
     public Rectangle collisionShapeTop;
     public Rectangle collisionShapeBottom;
 
     public Character(TextureRegion currentFrame) {
-        this.currentFrame = currentFrame;
-        this.position = new Vector2(0, 0);
+        super(currentFrame);
         this.velocity = new Vector2(0, 0);
         createCollisionShapes();
     }
 
     public Character(TextureRegion currentFrame, Vector2 position) {
-        this.currentFrame = currentFrame;
-        this.position = position;
+        super(currentFrame, position);
         this.velocity = new Vector2(0, 0);
         createCollisionShapes();
     }
 
     public Character(TextureRegion currentFrame, Vector2 position, Vector2 velocity) {
-        this.currentFrame = currentFrame;
-        this.position = position;
+        super(currentFrame, position);
         this.velocity = velocity;
         createCollisionShapes();
     }
     
     private void createCollisionShapes() {
-        this.collisionShape = new Rectangle(position.x, position.y, currentFrame.getRegionWidth(), currentFrame.getRegionHeight());
+        createCollisionShape();
         this.collisionShapeRight = new Rectangle(position.x + (currentFrame.getRegionWidth()/2f), position.y + (currentFrame.getRegionHeight()/4f), currentFrame.getRegionWidth()/2f, currentFrame.getRegionHeight()/2f);
         this.collisionShapeLeft = new Rectangle(position.x, position.y + (currentFrame.getRegionHeight()/4f), currentFrame.getRegionWidth()/2f, currentFrame.getRegionHeight()/2f);
         this.collisionShapeTop = new Rectangle(position.x + (currentFrame.getRegionWidth()/4f), position.y + (currentFrame.getRegionHeight()/2f), currentFrame.getRegionWidth()/2f, currentFrame.getRegionHeight()/2f);
         this.collisionShapeBottom = new Rectangle(position.x + (currentFrame.getRegionWidth()/4f), position.y, currentFrame.getRegionWidth()/2f, currentFrame.getRegionHeight()/2f);        
-    }
-
-    public float getWidth() {
-        return currentFrame.getRegionWidth();
-    }
-
-    public float getHeight() {
-        return currentFrame.getRegionHeight();
     }
 
     public void move(float x, float y) {
---
+++ core/src/main/java/com/sanvalero/aa2pmdm/entity/Entity.java
@@ -0,0 +1,36 @@
+package com.sanvalero.aa2pmdm.entity;
+
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+
+import lombok.Data;
+
+@Data
+public class Entity {
+    protected TextureRegion currentFrame;
+    protected Vector2 position;
+    public Rectangle collisionShape;
+
+    public Entity(TextureRegion currentFrame) {
+        this.currentFrame = currentFrame;
+        this.position = new Vector2(0, 0);
+    }
+
+    public Entity(TextureRegion currentFrame, Vector2 position) {
+        this.currentFrame = currentFrame;
+        this.position = position;
+    }
+
+    public void createCollisionShape() {
+        this.collisionShape = new Rectangle(position.x, position.y, currentFrame.getRegionWidth(), currentFrame.getRegionHeight());
+    }
+
+    public float getWidth() {
+        return currentFrame.getRegionWidth();
+    }
+
+    public float getHeight() {
+        return currentFrame.getRegionHeight();
+    }
+}
